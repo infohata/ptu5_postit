6eaf3e4d8afeb36b9a447adaa5f6c180caab11e7
Kestutis Januskevicius <infohata@gmail.com>
2022-11-24 11:21:17 +0200

    posts/ api test example

--- ptu5_postit/postit_api/models.py
+++ ptu5_postit/postit_api/models.py
@@ -22,3 +22,6 @@ class Post(models.Model):
             user=self.user,
             created_at=self.created_at,
         )
+
+    class Meta:
+        ordering = ('-created_at',)
--- ptu5_postit/postit_api/views.py
+++ ptu5_postit/postit_api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics
+from . import models, serializers
 
-# Create your views here.
+
+class PostList(generics.ListAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
