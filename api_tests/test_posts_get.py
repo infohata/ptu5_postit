912d3f8739b30b4c8288eff3ba64152e659f771c
Kestutis Januskevicius <infohata@gmail.com>
2022-11-24 12:02:18 +0200

    changed view to CreateListAPI

--- api_tests/test_posts_get.py
+++ api_tests/test_posts_get.py
@@ -1,5 +1,4 @@
 import requests
-from pprint import pprint
 
 reqUrl = "http://127.0.0.1:8000/posts/"
 
@@ -12,4 +11,4 @@ payload = ""
 
 response = requests.request("GET", reqUrl, data=payload,  headers=headersList)
 
-pprint(response.text)
+print(response.text)
