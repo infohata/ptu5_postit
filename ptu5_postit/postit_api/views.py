cf9bb1dc73a4b36de96ed335323c869a4285fd7a
Kestutis Januskevicius <infohata@gmail.com>
2022-11-25 12:11:13 +0200

    Comment model, admin, API

--- ptu5_postit/postit_api/views.py
+++ ptu5_postit/postit_api/views.py
@@ -32,3 +32,37 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
             return self.update(request, *args, **kwargs)
         else:
             raise ValidationError(_('You cannot change posts not of your own.'))
+
+
+class CommentList(generics.ListCreateAPIView):
+    # queryset = models.Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        post = models.Post.objects.get(pk=self.kwargs['pk'])
+        serializer.save(user=self.request.user, post=post)
+
+    def get_queryset(self):
+        post = models.Post.objects.get(pk=self.kwargs['pk'])
+        return models.Comment.objects.filter(post=post)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        comment = models.Comment.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if comment.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError(_('You cannot delete comments not of your own.'))
+
+    def put(self, request, *args, **kwargs):
+        comment = models.Comment.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if comment.exists():
+            return self.update(request, *args, **kwargs)
+        else:
+            raise ValidationError(_('You cannot change comments not of your own.'))
