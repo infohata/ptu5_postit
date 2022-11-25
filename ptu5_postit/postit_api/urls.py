6b8ae5ff64decea687c1190afd0c1927888f6c60
Kestutis Januskevicius <infohata@gmail.com>
2022-11-25 10:29:41 +0200

    CRUD API for Post done

--- ptu5_postit/postit_api/urls.py
+++ ptu5_postit/postit_api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('posts/', views.PostList.as_view()),
+    path('post/<int:pk>/', views.PostDetail.as_view()),
 ]
--- ptu5_postit/postit_api/views.py
+++ ptu5_postit/postit_api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from . import models, serializers
 
 
@@ -10,3 +12,23 @@ class PostList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        post = models.Post.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError(_('You cannot delete posts not of your own.'))
+    
+    def put(self, request, *args, **kwargs):
+        post = models.Post.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if post.exists():
+            return self.update(request, *args, **kwargs)
+        else:
+            raise ValidationError(_('You cannot change posts not of your own.'))
