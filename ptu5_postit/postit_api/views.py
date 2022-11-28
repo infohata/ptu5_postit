af4dd1d2ab6b7d579e3ca10623b37ce5c3e804db
Kestutis Januskevicius <infohata@gmail.com>
2022-11-28 15:00:25 +0200

    auth tokens, delete user

--- ptu5_postit/postit_api/views.py
+++ ptu5_postit/postit_api/views.py
@@ -103,3 +103,11 @@ class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
     permission_classes = [permissions.AllowAny]
+
+    def delete(self, request, *args, **kwargs):
+        user = User.objects.filter(pk=request.user.pk)
+        if user.exists():
+            user.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError(_('User does not exist.'))
